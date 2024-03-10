d4038e168b1c4ca5dad0dcc00e18ce72f8a4aaa4
gaurav-bhadane <gauravpbhadane2304@gmail.com>
2024-03-11 00:41:09 +0530

    String Input

---
+++ Strings/string_basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<<"What is your name: ";
+    getline(cin,name);
+    cout<<name;
+    return 0;
+}
